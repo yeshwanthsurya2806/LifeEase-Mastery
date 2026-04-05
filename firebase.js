a1a7884bb03f4c11880085ff80c2dfc83f7d87ec
Yeshwanth Surya <yeshwanth.surya2806@gmail.com>
2026-04-05 13:17:38 +0530

    Upgrade Firebase SDK and add Firestore support
    Updated Firebase SDK imports to version 10.7.1 and added Firestore initialization.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBY1lqKwZ_ACKs5NZqHzR88NNzAL4ZsYUk",
@@ -14,11 +9,11 @@ const firebaseConfig = {
   projectId: "lifeease-mastery",
   storageBucket: "lifeease-mastery.firebasestorage.app",
   messagingSenderId: "563103595929",
-  appId: "1:563103595929:web:68ec094cdd5c339c7ae708"
+  appId: "1:563103595929:web:68ec094cdd5c339c7ae708",
+  measurementId: "G-ERQRPD6MQ3"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export { signInWithPopup, signOut, onAuthStateChanged };
+export const db = getFirestore(app);
+
